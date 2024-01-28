dfe5e9b72ce7366cac9df7263c1ca04d5d8fea54
Aaron Mathews <115499223+aaronmathews773@users.noreply.github.com>
2024-01-28 02:40:03 +0000

    Exploring flights data

--- flightBuddy/api_exploration/main.py
+++ flightBuddy/api_exploration/main.py
@@ -1,8 +1,14 @@
 import requests
+import pandas
 
-flight_api_url = 'https://flight-buddy-ec24703f32e0.herokuapp.com/airports?code=DFW'
+flight_api_url = 'https://flight-buddy-ec24703f32e0.herokuapp.com/'
 
 
 # Getting flight data for DFW
-response = requests.get(flight_api_url)
+response = requests.get(flight_api_url + 'airports?code=DFW')
+print(response.json())
+
+# Getting flight data by day
+response = requests.get(flight_api_url + 'flights?date=2017-08-29')
+flight_data = pd.read_csv()
 print(response.json())

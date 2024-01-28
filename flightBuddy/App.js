f3abf8b2d8c0a375afb405de198710198abca800
rushil.jayant@gmail.com <rushil.jayant@gmail.com>
2024-01-27 18:58:52 -0600

    Example component "big.jsx"

--- flightBuddy/App.js
+++ flightBuddy/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Big from './components/big.jsx';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Big>Hello World!!</Big>
       <StatusBar style="auto" />
     </View>
   );
---
+++ flightBuddy/components/big.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+
+const Big = () => {
+    return (
+        <Text style={{fontWeight: 'bold'}}>Hello World!!!</Text>
+    )
+}
+
+export default Big;

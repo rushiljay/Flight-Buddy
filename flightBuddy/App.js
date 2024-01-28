7b952b0be539171da27c30553b066b9f7157d879
Aaron Mathews <115499223+aaronmathews773@users.noreply.github.com>
2024-01-28 05:18:11 +0000

    Added basic UI with title and scroll view.

--- flightBuddy/App.js
+++ flightBuddy/App.js
@@ -5,10 +5,17 @@ import Big from './components/big.jsx';
 
 export default function App() {
   return (
+    <ScrollView>
+      <Text style={styles.title}>Attendr</Text>
+
+      
     <View style={styles.container}>
       <Big>Hello World!!</Big>
+
+      <TextInput defaultValue="Hello, I am your digital flight assistant! Let me know what you need, and I will be happy to help!"/>
       <StatusBar style="auto" />
     </View>
+    </ScrollView>
   );
 }
 
@@ -19,4 +26,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 50,
+    margin: 45,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  }
+
 });
